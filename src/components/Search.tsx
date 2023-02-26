89fe615af0e1a6a2d180e435beac674ba7989568
akwasi kusi-appiah <akappiah@gmail.com>
2023-02-26 07:46:08 +0000

    added styles to all components- header, search and countries

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Search = () => {
   return (
     <div>
-      <section className='findBar'>
-        <div className='find_input'>
-            <i className='iSearch'></i>
-            <input type="search" name="filter" id="filter" placeholder="Search for a Country"/>
+      <section className='searchbar'>
+        <div className='search_filter'>
+            <i className='isearch'></i>
+            <input type="search" name="filter" id="input_filter" placeholder="Search for a Country"/>
         </div>
 
-        <div className='region_option'>
-            <select name='select' id='select' className='find_option'>
+        <div className='region_filter'>
+            <select name='select' id='select' className='select_region'>
                 <option value="Filter by Region">Filter by Region</option>
                 <option value="Africa">Africa</option>
                 <option value="America">America</option>
