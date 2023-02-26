501ef1baa3604da6be39e9911da7214675aa2ca6
akwasi kusi-appiah <akappiah@gmail.com>
2023-02-26 05:04:18 +0000

    added header and filter components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Search from './components/Search';
 function App() {
   return (
     <div className="App">
-      <Header />
+       <Header />
       <Search />
       <Countries />
     </div>
